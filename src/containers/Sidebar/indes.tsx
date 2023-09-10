4340ec9d04103dbf9769e31986eb23646a342120
Higor Santos <higor.stos@outlook.com>
2023-09-10 02:26:11 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/indes.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/higorstos.png" />
+    <Titulo fontSize={20}>Higor Santos</Titulo>
+  </aside>
+)
+
+export default Sidebar
